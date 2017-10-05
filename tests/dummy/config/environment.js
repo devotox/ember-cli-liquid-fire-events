18434a7cccc26c2c9e079acd1561edaff3b4499e
Devonte <devo.tox.89@gmail.com>
2017-10-05 09:52:42 +0100

    add github io

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,6 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-cli-liquid-fire-events/';
     // here you can enable a production-specific feature
   }
 
